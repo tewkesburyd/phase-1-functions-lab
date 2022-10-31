4cf9452da691ac294e75f5d3c47d5fc4437814a9
David Tewkesbury <tewkesburyd@hotmail.com>
2022-10-31 16:12:07 -0600

    commit tree a4b095f61fba05454f0cf92e7719660b4199e3f1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (street){
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street){
+    return Math.abs(street - 42) * 264;
+} 
+
+function distanceTravelledInFeet(start, finish){
+    return Math.abs(start - finish) * 264
+}
+
+function calculatesFarePrice(start, finish){
+    //let fare;
+    let dist = Math.abs(start - finish) * 264
+    if (dist <= 400 ){
+        return 0;
+    } else if (dist > 400 && dist <= 2000){
+        return (dist - 400) * .02;
+    } else if (dist > 2000 && dist <= 2500){
+        return 25.00;
+    } else if (dist > 2500){
+        return 'cannot travel that far';
+    }
+}
